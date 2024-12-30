a081a74e4b49f4194b8c0d8e5dcfd42fa25ba298
voinov <voinov@skbkontur.ru>
2024-12-30 14:30:41 +0500

    refactor v2

--- src/components/city-offers/components/no-offers-slug/index.tsx
+++ src/components/city-offers/components/no-offers-slug/index.tsx
@@ -5,7 +5,7 @@ export const NoOffersSlug = () => (
         <div className="cities__status-wrapper tabs__content">
           <b className="cities__status">No places to stay available</b>
           <p className="cities__status-description">We could not find any property available at the moment in
-            Dusseldorf
+            Paris
           </p>
         </div>
       </section>
